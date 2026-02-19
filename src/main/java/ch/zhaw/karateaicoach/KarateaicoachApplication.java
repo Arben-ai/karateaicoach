d08d629f4b3cfcf666d7305de000c18e1c85b8e1
Arben-ai <mustaarb@students.zhaw.ch>
2026-02-19 16:29:27 +0100

    first commit

---
+++ src/main/java/ch/zhaw/karateaicoach/KarateaicoachApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.karateaicoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateaicoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateaicoachApplication.class, args);
+	}
+
+}
